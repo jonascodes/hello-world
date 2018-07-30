ae564335933a44e0a80e4f391baf273627d422f4
Jonas Huenten <huenten@gmx.de>
2018-07-30 15:10:50 +0200

    bootstrap start

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
-
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-  <script src="js/scripts.js"></script>
-  Hello World - this file was edited in Atom, merged to the github repo and then fetched by the webserver from the github repo.
+
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This is some text.</p>
+</div>
+
 </body>
 </html>
